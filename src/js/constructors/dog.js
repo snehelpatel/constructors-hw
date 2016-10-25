24e3f1397ad69e160408998dbf5eda4c18a90c20
Snehel Patel <snehelpatel92@gmail.com>
2016-10-25 19:55:45 -0400

    clean up and adding comments' npm run deploy ' '

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -2,11 +2,11 @@ function Dog (options) {
 let opts = options || {};
 this.status = opts.status || "normal";
 this.color = opts.color || "black";
-// Sadie as Mason's
+// All instances of dog have no owner properties, which reads as undefined? Need to give Sadie owner === 'mason' while leaving the other dog's undefined.  
 if (opts.owner === 'mason') {
   this.owner = 'sadie'
 } else { opts.owner}
-//If Sadie's hunger is set to false, keept it that way and set the others to true??
+//Since Sadie's hunger is set to false, keept it that way and set the others to true??
 if (opts.hungry === false) {
     this.hungry = false
   } else{ this.hungry = true
